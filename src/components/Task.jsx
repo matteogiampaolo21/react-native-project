e2e2f818e5bdecabe32b749b38f4d97305561383
Matteo Giampaolo <doyouthinkysauros@gmail.com>
2023-12-14 10:36:57 +0700

    fixed small issue with long titles and bodys in tasks. Approving a task will now update without needing to refresh.

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -9,6 +9,8 @@ import { Feather } from '@expo/vector-icons';
 export const Task = ({title,body,priority,taskID,isCompleted,accessControl,tasks,setTasks}) => {
 
     const [isCompletedIcons, setCompletedIcons] = useState(isCompleted)
+
+    
     const updateTask = (field) => {
         
         if (isCompletedIcons){
@@ -65,6 +67,10 @@ export const Task = ({title,body,priority,taskID,isCompleted,accessControl,tasks
                 text:'Confirm',
                 onPress:async () => {
                     console.log("Confirmed Action")
+                    await setTasks((prevState) => {
+                        const result = prevState.filter((task) => task.id !== taskID)
+                        return [...result]
+                    })
                     await deleteDoc(doc(db, "tasks", taskID));
                 }
             },
@@ -72,13 +78,13 @@ export const Task = ({title,body,priority,taskID,isCompleted,accessControl,tasks
     }
 
     return (
-        <View style={{flex:1,flexDirection:'row',justifyContent:'space-between',alignItems:'center',backgroundColor:"#404040", marginTop:20,marginHorizontal:20,borderColor:(isCompletedIcons ? '#22c55e' : 'black'), paddingVertical:5,paddingHorizontal:10,borderRadius:5,borderWidth:1,}}>
-            <View>
-                <Text style={{color:'white',fontSize:17}}>{title}</Text>
+        <View style={{flex:4,flexDirection:'row',justifyContent:'space-between',alignItems:'center',backgroundColor:"#404040", marginTop:20,marginHorizontal:20,borderColor:(isCompletedIcons ? '#22c55e' : 'black'), paddingVertical:5,paddingHorizontal:10,borderRadius:5,borderWidth:1,}}>
+            <View style={{flex:3}}>
+                <Text style={{color:'white',fontSize:17,fontWeight:700}}>{title}</Text>
                 <Text style={{color:'#d4d4d4',fontSize:15}}>Priority : <Text style={{fontWeight:700}}>{priority}</Text></Text>
                 <Text style={{color:'#d4d4d4',fontSize:15}}>{body}</Text>
             </View>
-            <View style={{display:'flex',flexDirection:'row'}}>
+            <View style={{display:'flex',flexDirection:'row',flex:1,justifyContent:'flex-end'}}>
                 {isCompletedIcons && accessControl ?
                     <TouchableOpacity onPress={() => approveTask()}>
                         <Feather style={{marginRight:10}} name={'thumbs-up'} size={30} color='#737373'/>
